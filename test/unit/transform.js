f78bd64f4f86242e869335e457dbe3c7b42dfdfd
Andy Armstrong <andy@hexten.net>
2018-07-24 17:14:44 +0100

    Matrix based transform.

---
+++ test/unit/transform.js
@@ -0,0 +1,95 @@
+"use strict";
+
+const chai = require("chai");
+const expect = chai.expect;
+
+const Transform = require("../../webapp/lib/js/transform.js")
+
+const testPos = [
+  [0, 0],
+  [0, 1],
+  [0, 2],
+  [0, -1],
+  [-1, 0],
+  [1, 0],
+  [-1, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [200, -3],
+  [11, 11],
+  [-1, 1000]
+];
+
+function offsetPairs(list, dx, dy) {
+  return list.map(xy => {
+    const [x, y] = xy;
+    return [x + dx, y + dy];
+  });
+}
+
+function scale(list, sx, sy) {
+  return list.map(xy => {
+    const [x, y] = xy;
+    return [x * sx, y * sy]
+  });
+}
+
+function swapPairs(list) {
+  return list.map(xy => {
+    const [x, y] = xy;
+    return [y, x]
+  });
+}
+
+function testTransform(t, src, want) {
+  const got = t.xys(src);
+  // console.log({ src, want, got });
+  expect(got)
+    .to.deep.equal(want);
+  expect(t.inverse.xys(want))
+    .to.deep.equal(src);
+}
+
+describe.only("Transform", () => {
+  describe("identity", () => {
+    describe("implicit identity", () => {
+      const t = new Transform();
+      it("should not change coordinates", () => {
+        testTransform(t, testPos, testPos);
+      });
+    });
+
+    describe("explicit identity", () => {
+      const t = new Transform(0, 0, "across");
+      it("should not change coordinates", () => {
+        testTransform(t, testPos, testPos);
+      });
+    });
+
+    describe("origin", () => {
+      const t = new Transform(3, 4, "across");
+      it("should offset coordinates", () => {
+        testTransform(t, testPos, offsetPairs(testPos, 3, 4));
+      });
+    });
+
+    describe("flipped (constructor)", () => {
+      const t = new Transform(7, 2, "down");
+      it("should flip coordinates", () => {
+        testTransform(t, testPos, offsetPairs(swapPairs(testPos),
+          7, 2));
+      });
+    });
+
+    describe("flipped (version)", () => {
+      const t = new Transform(3, 4, "across")
+        .flipped;
+      it("should flip coordinates", () => {
+        testTransform(t, testPos, offsetPairs(swapPairs(testPos),
+          3, 4));
+      });
+    });
+
+  });
+});
---
+++ webapp/lib/js/transform.js
@@ -0,0 +1,52 @@
+const Matrix = require("transformation-matrix-js")
+  .Matrix;
+
+class Transform {
+  static decodeDirection(dir) {
+    switch (dir || "across") {
+      case "across":
+        return [1, 0, 0, 1];
+      case "down":
+        return [0, 1, 1, 0];
+      default:
+        throw new Exception("Bad play direction: " + dir);
+    }
+  }
+
+  constructor(cx, cy, dir) {
+    if (cx === undefined) {
+      this.m = new Matrix();
+    } else if (cx instanceof Matrix) {
+      this.m = cx;
+    } else {
+      const mat = Transform.decodeDirection(dir);
+      this.m = Matrix.from(...mat, cx, cy);
+    }
+  }
+
+  xy(x, y) {
+    const pt = this.m.applyToPoint(x, y);
+    return [pt.x, pt.y].map(Math.round);
+  }
+
+  xys(list) {
+    return list.map(xy => {
+      const [x, y] = xy;
+      return this.xy(x, y)
+    });
+  }
+
+  get inverse() {
+    return this._inverse = this._inverse ||
+      new Transform(this.m.inverse());
+  }
+
+  get flipped() {
+    if (this._flipped) return this._flipped;
+    let m = this.m.clone();
+    m.transform(0, 1, 1, 0, 0, 0);
+    return this._flipped = new Transform(m);
+  }
+}
+
+module.exports = Transform;
