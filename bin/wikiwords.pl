81cb4124169c895b1fdc5bf655f4566d30f6af6c
Andy Armstrong <andy@hexten.net>
2018-08-06 19:25:46 +0100

    Extract wiki words

--- bin/wikiwords.pl
+++ bin/wikiwords.pl
@@ -8,22 +8,96 @@ use warnings;
 
 use lib qw( lib );
 
+use File::Find;
 use JSON ();
 use Path::Class;
-use WordIX::XML::WikiParser;
-use XML::SAX;
+
+$| = 1;
 
 STDOUT->binmode(":utf8");
 
-my $handler = WordIX::XML::WikiParser->new(
-  text => sub {
-    my $text = shift;
-    say $text;
+=for ref
+
+<doc id="57943569" url="https://en.wikipedia.org/wiki?curid=57943569" title="Ephraim Bagenda">
+
+=cut
+
+my ( $words, @src ) = @ARGV;
+
+say "Loading dictionary $words";
+my $stats
+ = { map { $_ => { freq => 0, articles => 0 } } load_dict($words) };
+
+find {
+  wanted => sub {
+    return unless -f;
+    my $file = $_;
+    say "Reading $file";
+    load_articles(
+      $file,
+      sub {
+        my ( $title, @txt ) = @_;
+        scan_article( $stats, $title, @txt );
+      }
+    );
+  },
+  no_chdir => 1
+}, @src;
+
+my @by_freq
+ = map { $_->[0] }
+ sort  { $a->[1] <=> $b->[1] }
+ map { [$_, $stats->{$_}{freq}] } keys %$stats;
+
+for my $word (@by_freq) {
+  my $st = $stats->{$word};
+  say join ",", $word, $st->{freq}, $st->{articles};
+}
+
+sub scan_article {
+  my ( $stats, $title, @txt ) = @_;
+  # say $title;
+  my %wc = ();
+  for my $word ( map { clean_word($_) } map { split /\s+/ } @txt ) {
+    my $st = $stats->{$word};
+    next unless $st;
+    $st->{freq}++;
+    $wc{$word}++;
   }
-);
+  $stats->{$_}{articles}++ for keys %wc;
+}
 
-my $parser = XML::SAX::ParserFactory->parser( Handler => $handler );
+sub clean_word {
+  my $word = shift;
+  $word =~ s/[)"'.,;:?]+$//;
+  $word =~ s/^[("']+//;
+  return () unless $word =~ /^[a-z]{2,}$/i;
+  return uc $word;
+}
 
-$parser->parse_file(*STDIN);
+sub load_articles {
+  my ( $file, $cb ) = @_;
+  my ( $in_doc, @doc );
+  for my $ln ( file($file)->slurp ) {
+    if ( $ln =~ /^\<doc\s+id="\d+"\s+url=".+?"\s+title="(.+?)">/ ) {
+      $in_doc = $1;
+      # say "  $in_doc";
+      next;
+    }
+    if ( defined $in_doc ) {
+      if ( $ln =~ m{^</doc>} ) {
+        $cb->( $in_doc, splice @doc ) if @doc;
+        undef $in_doc;
+        next;
+      }
+      push @doc, $ln;
+    }
+  }
+}
+
+sub load_dict {
+  my $words = file(shift)->slurp;
+  return map uc, split /\s+/, $words;
+}
 
 # vim:ts=2:sw=2:sts=2:et:ft=perl
