febd6e7a714f29d720587edca2cbecb8dfd54bfe
Andy Armstrong <andy@hexten.net>
2018-08-09 13:02:49 +0100

    Tidied.

--- webapp/lib/js/bag.js
+++ webapp/lib/js/bag.js
@@ -2,40 +2,41 @@ const _ = require("lodash");
 const MW = require("mixwith");
 const Generation = require("./mixin/generation");
 
-class Bag extends MW.mix(Object).with(Generation)  {
-  constructor(tiles) {
-    super();
-    this.tiles = tiles || [];
-  }
+class Bag extends MW.mix(Object)
+  .with(Generation) {
+    constructor(tiles) {
+      super();
+      this.tiles = tiles || [];
+    }
 
-  get size() {
-    return this.tiles.length;
-  }
+    get size() {
+      return this.tiles.length;
+    }
 
-  add(...tiles) {
-    this.touch();
-    Array.prototype.push.apply(this.tiles, _.flattenDeep(tiles));
-  }
+    add(...tiles) {
+      this.touch();
+      Array.prototype.push.apply(this.tiles, _.flattenDeep(tiles));
+    }
 
-  pull(idx) {
-    this.touch();
-    return _.pullAt(this.tiles, idx);
-  }
+    pull(idx) {
+      this.touch();
+      return _.pullAt(this.tiles, idx);
+    }
 
-  take(n) {
-    const idx = _.shuffle(_.range(this.size));
-    return this.pull(idx.slice(0, n));
-  }
+    take(n) {
+      const idx = _.shuffle(_.range(this.size));
+      return this.pull(idx.slice(0, n));
+    }
 
-  fillFrom(bag, n) {
-    const need = n - this.size;
-    this.add(bag.take(need));
-  }
+    fillFrom(bag, n) {
+      const need = n - this.size;
+      this.add(bag.take(need));
+    }
 
-  toString() {
-    return this.tiles.map(x => x.toString())
-      .join(" | ");
+    toString() {
+      return this.tiles.map(x => x.toString())
+        .join(" | ");
+    }
   }
-}
 
 module.exports = Bag
--- webapp/lib/js/game.js
+++ webapp/lib/js/game.js
@@ -2,77 +2,77 @@ const _ = require("lodash")
 const MW = require("mixwith");
 
 const Player = require("./player")
-
 const Generation = require("./mixin/generation");
 
-class Game extends MW.mix(Object).with(Generation) {
-  constructor(opt) {
-    super();
-    Object.assign(this, {
-      players: 2,
-      next: 0
-    }, opt);
+class Game extends MW.mix(Object)
+  .with(Generation) {
+    constructor(opt) {
+      super();
+      Object.assign(this, {
+        players: 2,
+        next: 0
+      }, opt);
 
-    if (!this.rules)
-      throw new Error("Missing option: rules");
-    if (!this.trie)
-      throw new Error("Missing option: trie");
+      if (!this.rules)
+        throw new Error("Missing option: rules");
+      if (!this.trie)
+        throw new Error("Missing option: trie");
 
-    this._init();
-  }
-
-  _makePlayers(n) {
-    let pl = [];
-    for (let pn = 1; pn <= n; pn++) {
-      let player = new Player({
-        name: "Player " + pn
-      });
-      this.fillTray(player);
-      pl.push(player);
+      this._init();
     }
-    return pl;
-  }
 
-  _init() {
-    this.board = this.board || this.rules.makeBoard();
-    this.bag = this.bag || this.rules.makeBag();
+    _makePlayers(n) {
+      let pl = [];
+      for (let pn = 1; pn <= n; pn++) {
+        let player = new Player({
+          name: "Player " + pn
+        });
+        this.fillTray(player);
+        pl.push(player);
+      }
+      return pl;
+    }
 
-    if (_.isNumber(this.players))
-      this.players = this._makePlayers(this.players);
-    if (!_.isArray(this.players))
-      throw new Error("players must be a number or an array of players");
-  }
+    _init() {
+      this.board = this.board || this.rules.makeBoard();
+      this.bag = this.bag || this.rules.makeBag();
 
-  get canPlay() {
-    for (const p of this.players)
-      if (p.tray.size === 0)
-        return false;
-    return true;
-  }
+      if (_.isNumber(this.players))
+        this.players = this._makePlayers(this.players);
+      if (!_.isArray(this.players))
+        throw new Error("players must be a number or an array of players");
+    }
 
-  nextPlayer() {
-    const p = this.players[this.next++];
-    if (this.next == this.players.length)
-      this.next = 0;
+    get canPlay() {
+      for (const p of this.players)
+        if (p.tray.size === 0)
+          return false;
+      return true;
+    }
+
+    nextPlayer() {
+      const p = this.players[this.next++];
+      if (this.next == this.players.length)
+        this.next = 0;
       // Bump global generation
       this.nextGeneration();
-    return p;
-  }
+      return p;
+    }
 
-  fillTray(player) {
-    player.tray.fillFrom(this.bag, this.rules.traySize);
-  }
+    fillTray(player) {
+      player.tray.fillFrom(this.bag, this.rules.traySize);
+    }
 
-  sanityCheck() {
-    const words = this.board.words(this.rules.direction);
-    const invalid = words.filter(w => !this.trie.valid(w));
-    if (invalid.length)
-      throw new Error("Illegal words: " + invalid.join(", "));
-  }
+    sanityCheck() {
+      const words = this.board.words(this.rules.direction);
+      const invalid = words.filter(w => !this.trie.valid(w));
+      if (invalid.length)
+        throw new Error("Illegal words: " + invalid.join(", "));
+    }
 
-  get words() {
-    return this.board.words(this.rules.direction);
+    get words() {
+      return this.board.words(this.rules.direction);
+    }
   }
-}
 
 module.exports = Game;
