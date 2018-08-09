8a8aa497422db80793a43a3d606b288d8ff7bdff
Andy Armstrong <andy@hexten.net>
2018-08-09 12:38:34 +0100

    Tidy.

--- webapp/lib/js/player.js
+++ webapp/lib/js/player.js
@@ -3,13 +3,14 @@ const MW = require("mixwith");
 const Bag = require("./bag")
 const Generation = require("./mixin/generation");
 
-class Player extends MW.mix(Object).with(Generation) {
-  constructor(opt) {
-    super();
-    Object.assign(this, {}, opt);
-    this.tray = this.tray || new Bag;
-    this.score = 0;
+class Player extends MW.mix(Object)
+  .with(Generation) {
+    constructor(opt) {
+      super();
+      Object.assign(this, {}, opt);
+      this.tray = this.tray || new Bag;
+      this.score = 0;
+    }
   }
-}
 
 module.exports = Player;
