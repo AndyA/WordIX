bea63f21db99b686efe3bb66dea973916735b48a
Andy Armstrong <andy@hexten.net>
2018-08-09 12:28:46 +0100

    TIdy up.

--- webapp/lib/js/mixin/generation.js
+++ webapp/lib/js/mixin/generation.js
@@ -3,14 +3,12 @@
 const MW = require("mixwith");
 
 const Generation = MW.Mixin((superclass) => class extends superclass {
-  nextGeneration() {
-    // make default value
-    this.generation;
-    return ++this._generation;
-  }
-
   get generation() {
     return this._generation = this._generation || 0;
+  }
+
+  nextGeneration() {
+    return this._generation = this.generation + 1;
   }
 });
 
